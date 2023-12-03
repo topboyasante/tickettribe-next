8ef5d314a6989348fe86441c0b3368e16de22ed0
Nana K. <73991180+topboyasante@users.noreply.github.com>
2023-12-03 19:53:06 +0000

    feat: Set Up Dark Mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      
+      colors: {
+        "primary-light": "#d80032",
+        "primary-dark": "#fcd581",
+      },
     },
   },
   plugins: [],
-}
-export default config
+  darkMode: "class",
+};
+export default config;
