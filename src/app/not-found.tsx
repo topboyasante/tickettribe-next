c101f9d82553a4b52fe33d5803831d70d691d1a7
Nana K. <73991180+topboyasante@users.noreply.github.com>
2023-12-13 14:39:37 +0000

    Deployment fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ function NotFound() {
   return (
     <section className="w-full h-screen">
       <section className="flex justify-center items-center h-full">
-        <p>404 | The page you're looking for does not exist.</p>
+        <p>404 | The page you&apos;re looking for does not exist.</p>
       </section>
     </section>
   );
